c7949ec8f1630651e7751c47b96b9a52dd719309
diabyh599-gif <diabyh599@gmail.com>
2026-08-03 23:09:28 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const briefcase = document.getElementById("briefcase");
+    const form = document.getElementById("register-container");
 
+    // Faire apparaître la mallette
     setTimeout(() => {
-        briefcase.style.left = "120px";
+        briefcase.classList.add("show");
     }, 500);
+
+    // Afficher le formulaire ensuite
+    setTimeout(() => {
+        form.classList.add("show");
+    }, 1800);
 
 });
