134030ea9bcc36914f0d4253237b5d899eeb69fe
diabyh599-gif <diabyh599@gmail.com>
2026-08-03 23:02:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("register-container");
     const briefcase = document.getElementById("briefcase");
 
-    briefcase.style.transform = "scale(0)";
-
     setTimeout(() => {
-        briefcase.style.transform = "scale(1)";
+        briefcase.classList.add("show");
     }, 500);
 
     setTimeout(() => {
