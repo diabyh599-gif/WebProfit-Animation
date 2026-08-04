21cb70cb5bd9a809d40dfdc739b96ab97c730b22
diabyh599-gif <diabyh599@gmail.com>
2026-08-04 15:44:10 +0000

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,6 +1,29 @@
 const tl = gsap.timeline({
     defaults: {
-        duration: 0.8,
         ease: "power2.out"
     }
+});
+
+tl
+
+.from("#character", {
+    x: -500,
+    duration: 2
+})
+
+.from("#briefcase", {
+    opacity: 0,
+    scale: 0.4,
+    duration: 0.8
+}, "-=1.2")
+
+.to("#briefcase", {
+    y: 45,
+    duration: 0.5
+})
+
+.from(".register-card", {
+    opacity: 0,
+    scale: 0.8,
+    duration: 0.7
 });
